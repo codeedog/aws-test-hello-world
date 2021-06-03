6b427c984c17eae171e9639f69b1b435a17fbfb0
Andrew Philips <andy@sweetsoftware.com>
2021-06-03 16:44:19 -0700

    modified lambda

--- lib/hw_cdk-stack.ts
+++ lib/hw_cdk-stack.ts
@@ -1,9 +1,17 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
 
 export class HwCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const helloWorldFunction = new lambda.Function(this, "HelloWorldFunction", {
+      code: lambda.Code.fromAsset("functions/helloworld"),
+      handler: "index.handler",
+      runtime: lambda.Runtime.NODEJS_12_X,
+      description:
+      "This Lambda Function returns the message: 'Hello World!' in the response body",
+    });
   }
 }
